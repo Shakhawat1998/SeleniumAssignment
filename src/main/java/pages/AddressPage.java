7ff203ca58fba198ad2df1f6d81cd17ccecee4e0
Shakhawat1998 <shopicuet17@gmail.com>
2025-11-06 09:26:09 +0600

    Added MyAccountPage and AddressPage to pages

---
+++ src/main/java/pages/AddressPage.java
@@ -0,0 +1,70 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.Select;
+
+public class AddressPage extends BasePage {
+
+    @FindBy(xpath = "//h1[text()='My account - Add new address']")
+    WebElement addNewAddressHeading;
+
+    @FindBy(id = "Address_FirstName")
+    WebElement firstNameInput;
+
+    @FindBy(id = "Address_LastName")
+    WebElement lastNameInput;
+
+    @FindBy(id = "Address_Email")
+    WebElement emailInput;
+
+    @FindBy(id = "Address_CountryId")
+    WebElement countryDropdown;
+
+    @FindBy(id = "Address_StateProvinceId")
+    WebElement stateDropdown;
+
+    @FindBy(id = "Address_City")
+    WebElement cityInput;
+
+    @FindBy(id = "Address_Address1")
+    WebElement address1Input;
+
+    @FindBy(id = "Address_ZipPostalCode")
+    WebElement zipCodeInput;
+
+    @FindBy(id = "Address_PhoneNumber")
+    WebElement phoneNumberInput;
+
+    @FindBy(xpath = "//button[text()='Save']")
+    WebElement saveButton;
+
+    public AddressPage(WebDriver driver) {
+        super(driver);
+    }
+
+    public boolean isAddNewAddressHeadingVisible() {
+        return addNewAddressHeading.isDisplayed();
+    }
+
+    public void fillAddressForm(String firstName, String lastName, String email, String country, String state,
+                                String city, String address1, String zip, String phone) {
+
+        firstNameInput.sendKeys(firstName);
+        lastNameInput.sendKeys(lastName);
+        emailInput.sendKeys(email);
+
+        new Select(countryDropdown).selectByVisibleText(country);
+        new Select(stateDropdown).selectByVisibleText(state);
+
+        cityInput.sendKeys(city);
+        address1Input.sendKeys(address1);
+        zipCodeInput.sendKeys(zip);
+        phoneNumberInput.sendKeys(phone);
+    }
+
+    public void clickSaveButton() {
+        saveButton.click();
+    }
+}
---
+++ src/main/java/pages/MyAccountPage.java
@@ -0,0 +1,26 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class MyAccountPage extends BasePage {
+
+    @FindBy(xpath = "//h1[text()='My account - Customer info']")
+    WebElement myAccountHeading;
+
+    @FindBy(xpath = "(//a[normalize-space()='Addresses'])[1]")
+    WebElement addressLink;
+
+    public MyAccountPage(WebDriver driver) {
+        super(driver);
+    }
+
+    public boolean isMyAccountHeadingVisible() {
+        return myAccountHeading.isDisplayed();
+    }
+
+    public void clickAddressLink() {
+        addressLink.click();
+    }
+}
